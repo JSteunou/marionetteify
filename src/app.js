d086b98e6851200f6ff6f6ef1df558995eb3d4ac
Jérôme Steunou <jerome.steunou+github@gmail.com>
2014-03-14 18:47:04 +0100

    Move all app boostrap code in app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
+// Vendors
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
 var Marionette = require('backbone.marionette');
 
+
+// Local
+var app = require('./app');
 var TodoModule = require('./modules/todo/module');
 
 
+
+// app bootstrap
 var app = new Marionette.Application();
 app.module('todo', TodoModule);
+app.start();
+Backbone.history.start();
+
 
 
 module.exports = app;
