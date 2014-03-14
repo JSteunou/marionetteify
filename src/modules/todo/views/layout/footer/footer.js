ad48809d3cc4d56ba4d32678cae57de591e25f4f
Jérôme Steunou <jerome.steunou+github@gmail.com>
2014-03-14 20:53:46 +0100

    Add footer logic

--- src/modules/todo/views/layout/footer/footer.js
+++ src/modules/todo/views/layout/footer/footer.js
@@ -5,7 +5,59 @@ var tpl = require('./footer.hbs');
 
 
 module.exports = Marionette.ItemView.extend({
+    template: tpl,
 
-    template: tpl
+    ui: {
+        filters: '#filters a'
+    },
+
+    events: {
+        'click #clear-completed': 'onClearClick'
+    },
+
+    collectionEvents: {
+        'all': 'render'
+    },
+
+    templateHelpers: {
+        activeCountLabel: (this.activeCount === 1 ? 'item' : 'items') + ' left'
+    },
+
+
+
+    initialize: function () {
+        // this.listenTo(App.vent, 'todoList:filter', this.updateFilterSelection, this);
+    },
+
+    serializeData: function () {
+        var active = this.collection.getActive().length;
+        var total = this.collection.length;
+
+        return {
+            activeCount: active,
+            totalCount: total,
+            completedCount: total - active
+        };
+    },
+
+    onRender: function () {
+        this.$el.parent().toggle(this.collection.length > 0);
+        this.updateFilterSelection();
+    },
+
+    // TODO: move this to router or controller
+    updateFilterSelection: function () {
+        this.ui.filters
+            .removeClass('selected')
+            .filter('[href="' + (location.hash || '#') + '"]')
+            .addClass('selected');
+    },
+
+    onClearClick: function () {
+        var completed = this.collection.getCompleted();
+        completed.forEach(function (todo) {
+            todo.destroy();
+        });
+    }
 
 });
