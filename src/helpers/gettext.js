ed37a793f06b5d6968871e885c12fa31291c32be
Jérôme Steunou <jerome.steunou+github@gmail.com>
2014-03-23 20:08:26 +0100

    Prepare using gettext the traditional way

--- src/helpers/gettext.js
+++ src/helpers/gettext.js
@@ -1,6 +1,14 @@
 var Handlebars = require("hbsfy/runtime");
+var Jed = require('jed');
+
 var jedi18n = require("../services/jedi18n");
 
+
+
 Handlebars.registerHelper("gtt", function(singular, plural, n) {
-    return jedi18n.gtt(singular, plural, n);
+    if (plural) {
+        return Jed.sprintf(jedi18n.ngettext(singular, plural, n), n);
+    } else {
+        return jedi18n(singular);
+    }
 });
--- src/services/jedi18n.js
+++ src/services/jedi18n.js
@@ -7,24 +7,16 @@ var languageService = require('./language');
 
 
 var pojson = require('./translations/' + languageService.get());
-
-module.exports = {
-    i18n: new Jed(pojson),
-    /**
-     * Shortcut to Jed API
-     * @param  {String} singular    singular key
-     * @param  {String} [plural]    plural key
-     * @param  {Number} [n]         plural number. Mandatory if plural key.
-     * @return {String}             translation
-     */
-    gtt: function(singular, plural, n) {
-        var tr = this.i18n.translate(singular);
-        if (_.isString(plural) && _.isNumber(n)) {
-            return tr.ifPlural(n, plural).fetch(n);
-        } else {
-            return tr.fetch();
-        }
-    }
-};
+var jedi18n = new Jed(pojson);
 
 
+function gettext() {
+    jedi18n.gettext.apply(jedi18n, arguments);
+}
+
+gettext.gettext  = _.bind(jedi18n.gettext, jedi18n);
+gettext.ngettext = _.bind(jedi18n.ngettext, jedi18n);
+
+
+
+module.exports = gettext;
