240bb04f39af9c8d0bbf3388168a8621e442a6d7
Jérôme Steunou <jerome.steunou+github@gmail.com>
2014-03-23 21:10:49 +0100

    Make language service safer & regenerate pos

--- src/services/language.js
+++ src/services/language.js
@@ -18,17 +18,28 @@ var isAuthorized = function(lng) {
     return _.indexOf(config.languages, lng) > -1;
 };
 
+var normalize = function(lng) {
+    return (lng && lng.slice) ? lng.slice(0, 2) : '';
+};
+
 
 var language = {
 
     set: function(lng) {
+        lng = normalize(lng);
         if (isAuthorized(lng)) {
             localStorage.setItem(LS_KEY, lng);
         }
     },
 
     get: function() {
-        return localStorage.getItem(LS_KEY) || config.defaultLanguage || (config.languages && config.languages[0]) || 'en';
+        var lng = normalize(localStorage.getItem(LS_KEY));
+        if (isAuthorized(lng)) return lng;
+
+        lng = normalize(navigator.language || navigator.userLanguage);
+        if (isAuthorized(lng)) return lng;
+
+        return normalize(config.defaultLanguage || (config.languages && config.languages[0]));
     }
 
 };
